9a0969dc3d940f796647e28372d635e45f0f7599
jc_pc <juan.cruz.business@gmail.com>
2023-06-15 21:29:25 -0500

    d

---
+++ script.js
@@ -0,0 +1,18 @@
+function toggle(id) {
+    var element = document.getElementById(id);
+    if (element.classList.contains('hidden')) {
+        element.classList.remove('hidden');
+    } else {
+        element.classList.add('hidden');
+I apologize for the incomplete response. Here's the complete JavaScript code:
+
+JavaScript (script.js):
+```javascript
+function toggle(id) {
+    var element = document.getElementById(id);
+    if (element.classList.contains('hidden')) {
+        element.classList.remove('hidden');
+    } else {
+        element.classList.add('hidden');
+    }
+}
