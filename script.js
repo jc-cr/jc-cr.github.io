47405453e0a1997092f95574699c78c57e437519
jc-cr <juan.cruz.business@gmail.com>
2023-06-29 22:42:45 -0500

    removed stupid js

--- script.js
+++
@@ -1,8 +0,0 @@
-function toggle(id) {
-    var element = document.getElementById(id);
-    if (element.style.display === "none") {
-        element.style.display = "block";
-    } else {
-        element.style.display = "none";
-    }
-}
